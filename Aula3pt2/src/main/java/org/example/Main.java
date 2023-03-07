e309eda1b97900cb82954c1cd097528ed03260f5
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-07 14:58:20 -0300

    Coloquei comentarios para conseguir entender depois o que aconteceu

--- Aula3pt2/src/main/java/org/example/Main.java
+++ Aula3pt2/src/main/java/org/example/Main.java
@@ -8,9 +8,17 @@ public class Main {
     */
     public static void main(String[] args) {
 
+        // instanciando a var a1 como uma classe Arma
         Arma a1 = new Arma("Fogo", 300);
 
+        // instanciando a var n1 como uma classe Nave
         Nave n1 = new Nave("Falcon", 500, false, a1);
+
+        /*
+        * Observe que a var a1 foi passada para a var n1 como um argumento e levando consigo as info
+        que foram colodas na a1
+        * Isso ocorre pq foi definido na argumentacao desse metodo
+        */
 
         n1.MostraInfo();
     }
