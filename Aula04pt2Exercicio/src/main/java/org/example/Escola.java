26aebdcb5f603585cecaad9a1245a69c808acb76
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-14 15:07:07 -0300

    Corrigido os metodos da classe Escola e a programação da Main

--- Aula04pt2Exercicio/src/main/java/org/example/Escola.java
+++ Aula04pt2Exercicio/src/main/java/org/example/Escola.java
@@ -8,18 +8,66 @@ public class Escola {
     String endereco;    // endereco da escola
 
     // Array dessa classe
-    Estudante[] estudantes;     // edtudantes das escola
+    Estudante[] estudantes = new Estudante[20];     // estudantes das escola
 
     // metodos
     public void addEstudante(Estudante e){  // adicionar um estudante à escola
+
+        for (int i = 0; i < estudantes.length; i++){
+            if (estudantes[i] == null){
+                estudantes[i] = e;
+                System.out.println("Estudante matriculado!!!!");
+                break;
+            }
+        }
 
     }
 
     public void mostraInfo(){               // mostra as informaçõe dos estudantes
 
+        System.out.println("Nome da Escola: " + nome);  // mostra o nome da escola
+        System.out.println("----------------------------------");
+
+        for (int i = 0; i < estudantes.length; i++){
+            if (estudantes[i] != null){     // se não estiver null a memória mostra as informações
+                estudantes[i].mostraInfo();
+                System.out.println("----------------------------------");
+            }
+        }
+
     }
 
     public void QtdEstudantesPorAno(){      // mosta a quantidade de estudantes em cada ano letivo
+
+        System.out.println("----------------------------------");
+        System.out.println("ALUNOS DO PRIMEIRO ANO: ");
+        for (int i = 0; i < estudantes.length; i++) {
+            if (estudantes[i] != null) {
+                if (estudantes[i].ano.equals("PRIMEIRO")) {     // quer dizer que o aluno está no primeiro ano
+                    estudantes[i].mostraInfo();
+                }
+            }
+        }
+
+        System.out.println("----------------------------------");
+        System.out.println("ALUNOS DO SEGUNDO ANO: ");
+        for (int i = 0; i < estudantes.length; i++) {
+            if (estudantes[i] != null) {
+                if (estudantes[i].ano.equals("SEGUNDO")) {     // quer dizer que o aluno está no segundo ano
+                    estudantes[i].mostraInfo();
+                }
+            }
+        }
+
+        System.out.println("----------------------------------");
+        System.out.println("ALUNOS DO TERCEIRO ANO: ");
+        for (int i = 0; i < estudantes.length; i++) {
+            if (estudantes[i] != null) {
+                if (estudantes[i].ano.equals("TERCEIRO")) {     // quer dizer que o aluno está no terceiro ano
+                    estudantes[i].mostraInfo();
+                }
+            }
+        }
 
     }
 
--- Aula04pt2Exercicio/src/main/java/org/example/Main.java
+++ Aula04pt2Exercicio/src/main/java/org/example/Main.java
@@ -1,7 +1,74 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Declaração de Scanner
+        Scanner in = new Scanner(System.in);
+
+        // declaração do obejto escola e adicionando informações na mesma
+        Escola e1 = new Escola();
+        e1.nome = "Paulo Silveira";
+        e1.email = "PauloSilveira@gmail.com";
+        e1.endereco = "Santa Rita";
+        e1.numTelefone = 999999999;
+
+        // flag de controle de entrada de dados
+        boolean flag = true;
+
+        while (flag){   // enquanto a flag == true esse código vai rodar
+
+            System.out.println("BEM VINDO AO MENU DA ESCOLA: " + e1.nome);
+            System.out.println("1 - Matricular um novo aluno");
+            System.out.println("2 - Mostrar informações dos alunos");
+            System.out.println("3 - Mostrar quantos alunos em cada ano");
+            System.out.println("4 - Sair do menu");
+
+            int op;     // operador que determina o que será feito
+            op = in.nextInt();
+
+            switch (op){
+
+                case 1:     // matricular um aluno
+                    System.out.println("MATRICULE O NOVO ALUNO(A)");
+                    in.nextLine();
+
+                    System.out.println("Entre com o nome do aluno: ");  // adiciona o nome do estudante
+                    String nomeEstudante = in.nextLine();
+
+                    System.out.println("Entre com a idade do(a) " + nomeEstudante + ": ");  // adiciona a idade
+                    int idadeEstudante = in.nextInt();
+
+                    in.nextLine();
+                    System.out.println("Entre com o ano letivo do(a) " + nomeEstudante + ": ");   // adiciona o ano letivo
+                    String anoLetivo = in.nextLine();
+
+                    // instanciando o Estudante
+                    Estudante est = new Estudante(nomeEstudante, idadeEstudante, anoLetivo);
+                    e1.addEstudante(est);     // adiciona o estudante na escola
+                    break;
+
+
+                case 2:     // mostrar informações
+                    e1.mostraInfo();
+                    break;
+
+                case 3:    // mostra os alunos separados por ano letivo
+                    e1.QtdEstudantesPorAno();
+                    break;
+
+                case 4:     // sair do processo
+                    flag = false;
+                    break;
+
+                default:
+                    System.out.println("Entre com um número válido do menu!!!!");
+                    break;
+
+            }
+
+        }
+
     }
 }
