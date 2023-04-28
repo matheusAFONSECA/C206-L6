4eb833f64cf1847ec679c90984b20bc0ab146e8a
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-28 14:22:16 -0300

    Atributos e metodos colocados nas classes

--- Aula08_pt2/src/main/java/org/example/Carrinho.java
+++ Aula08_pt2/src/main/java/org/example/Carrinho.java
@@ -1,10 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // representa o objeto do carrinho
 public class Carrinho {
-    // atributos
-    private List<Produto> produtos
+    // ArrayList de produtos
+    private ArrayList<Produto> produtos = new ArrayList<>();
 
+    // metodos
+    public void addProduto(Produto produto){        // adiciona um produto no carrinho
+        produtos.add(produto);
+    }
+
+
+    // getters e setters
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
 }
--- Aula08_pt2/src/main/java/org/example/Main.java
+++ Aula08_pt2/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+
+// Exercicio sobre coleções
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
