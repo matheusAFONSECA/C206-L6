1f975953b6ff58ebd2818cfb544d6165c61c12fb
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-28 14:21:28 -0300

    Classes criadas

---
+++ Aula08_pt2/src/main/java/org/example/Carrinho.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+// representa o objeto do carrinho
+public class Carrinho {
+    // atributos
+    private List<Produto> produtos
+
+}
---
+++ Aula08_pt2/src/main/java/org/example/Produto.java
@@ -0,0 +1,31 @@
+package org.example;
+
+// classe que representa um produto
+public class Produto {
+    // atributos
+    private String nome;        // nome do produto
+    private double preco;       // preco do produto
+
+    // construtor
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
