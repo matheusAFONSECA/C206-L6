70474a634e7b6a8ace03005038ca64095ba7a8c3
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-06-20 20:46:46 -0300

    Inserção, Seleção, Atualização e Deleta de dados na tabela Aluno

--- Projeto/src/main/java/org/example/Main.java
+++ Projeto/src/main/java/org/example/Main.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import org.example.DAO.AlunoDAO;
+import org.example.MODEL.Aluno;
+
 // -> nessa classe vamos fazer o menu que vai rodar no termial e fazer todas as funcionalidades do código
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // testes de inserção, atualização, seleção e deletar dados
+        // definindo AlunoDAO
+        AlunoDAO alunoDAO = new AlunoDAO();
+
+        // definindo Aluno
+        Aluno aluno = new Aluno(1, "Marco", "GEC");
+
+        // inserindo o dado do aluno no BD
+        alunoDAO.insertAluno(aluno);
+
+        // atualizando o dado do aluno no BD
+        //alunoDAO.updateAluno(1, "GEC", aluno);
+
+        // deletando algum dado da tabela no BD
+        //alunoDAO.deleteAlno(1, "GEC");
+
+        // vendo os dados da tabela aluno
+        alunoDAO.selectAluno();
+
     }
 }
